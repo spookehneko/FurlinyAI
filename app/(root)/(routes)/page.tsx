0045f26e65346825798305ae2629a0fab13f4114
YoussefAyachi95 <youssef.ayachi@web.de>
2023-08-14 13:05:45 +0200

    Add companion list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,46 @@
+import Bots from "@/components/Bots";
 import Categories from "@/components/Categories"
 import SearchInput from "@/components/SearchInput"
 import prismadb from "@/lib/prismadb"
 
-async function RootPage() {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  }
+}
+
+async function RootPage({
+  searchParams
+}: RootPageProps) {
+  const data = await prismadb.bot.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        contains: searchParams.name,
+        mode: "insensitive"
+      }
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        }
+      }
+    }
+  })
+
+
   const categories = await prismadb.category.findMany()
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Bots data={data} />
     </div>
   )
 }
